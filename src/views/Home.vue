cff1b076944cf81efe716461486c2ca459753f97
Jim Strother <jim.strother@gmail.com>
2020-04-04 19:14:48 -0700

    Updated: App.vue & Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <button @click="toggleModal">Open</button>
+    <div v-if="isOpen" class="modal">
+      <button @click="toggleModal">Close</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggalModal() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
